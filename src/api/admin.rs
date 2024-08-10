1300a238166f3446601b098fe70891d7be31475c
dfunkt <daniel.barabasa@gmail.com>
2024-08-10 16:30:33 +0300

    Switch some links to personal repos
    - more accurate information, especially for the admin diagnostics page

--- src/api/admin.rs
+++ src/api/admin.rs
@@ -629,7 +629,7 @@ async fn get_text_api(url: &str) -> Result<String, Error> {
 }
 
 async fn has_http_access() -> bool {
-    let Ok(req) = make_http_request(Method::HEAD, "https://github.com/dani-garcia/vaultwarden") else {
+    let Ok(req) = make_http_request(Method::HEAD, "https://github.com/dfunkt/vaultwarden") else {
         return false;
     };
     match req.send().await {
@@ -648,13 +648,11 @@ async fn get_release_info(has_http_access: bool) -> (String, String, String) {
     // If the HTTP Check failed, do not even attempt to check for new versions since we were not able to connect with github.com anyway.
     if has_http_access {
         (
-            match get_json_api::<GitRelease>("https://api.github.com/repos/dani-garcia/vaultwarden/releases/latest")
-                .await
-            {
+            match get_json_api::<GitRelease>("https://api.github.com/repos/dfunkt/vaultwarden/releases/latest").await {
                 Ok(r) => r.tag_name,
                 _ => "-".to_string(),
             },
-            match get_json_api::<GitCommit>("https://api.github.com/repos/dani-garcia/vaultwarden/commits/main").await {
+            match get_json_api::<GitCommit>("https://api.github.com/repos/dfunkt/vaultwarden/commits/main").await {
                 Ok(mut c) => {
                     c.sha.truncate(8);
                     c.sha
@@ -663,8 +661,7 @@ async fn get_release_info(has_http_access: bool) -> (String, String, String) {
             },
             // Do not fetch the web-vault version when running within a container
             // The web-vault version is embedded within the container it self, and should not be updated manually
-            match get_json_api::<GitRelease>("https://api.github.com/repos/dani-garcia/bw_web_builds/releases/latest")
-                .await
+            match get_json_api::<GitRelease>("https://api.github.com/repos/dfunkt/bw_web_builds/releases/latest").await
             {
                 Ok(r) => r.tag_name.trim_start_matches('v').to_string(),
                 _ => "-".to_string(),
